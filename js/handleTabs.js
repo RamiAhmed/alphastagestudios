e094d0effb95c23ee77523a6cc4549f1ce047f6b
Rami Ahmed Bock <Rami_Ahmed00@hotmail.com>
2013-06-27 14:56:58 +0200

    Changed to tabbed pages
    - Removed all html files and added main.html
    - main.html uses tabbed pages with javascript to only show a portion of
    its content

---
+++ js/handleTabs.js
@@ -0,0 +1,67 @@
+
+var tabLinks = new Array();
+var contentSections = new Array();
+
+function initTabs() {
+    var tabListItems = document.getElementById('tabs').childNodes;
+    for (var i = 0; i < tabListItems.length; i++) {
+        if (tabListItems[i].nodeName == "LI") {
+            var tabLink = getFirstChildWithTagName(tabListItems[i], 'A');
+            var id = getHash(tabLink.getAttribute('href'));
+            tabLinks[id] = tabLink;
+            contentSections[id] = document.getElementById(id);
+        }
+    }
+
+    var i = 0;
+    for (var id in tabLinks) {
+        tabLinks[id].onclick = showTab;
+        tabLinks[id].onfocus = function() {
+            this.blur();
+        };
+
+        if (i == 0) {
+            tabLinks[id].className = 'active';
+        }
+
+        i++;
+    }
+
+    var i = 0;
+    for (var id in contentSections) {
+        if (i != 0) {
+            contentSections[id].className = 'tabContent hide';
+        }
+
+        i++;
+    }
+}
+
+function showTab() {
+    var selectedId = getHash(this.getAttribute('href'));
+
+    for (var id in contentSections) {
+        if (id == selectedId) {
+            tabLinks[id].className = 'active';
+            contentSections[id].className = 'tabContent';
+        }
+        else {
+            tabLinks[id].className = '';
+            contentSections[id].className = 'tabContent hide';
+        }
+    }
+
+    return false;
+}
+
+function getFirstChildWithTagName(element, tagName) {
+  for (var i = 0; i < element.childNodes.length; i++) {
+    if (element.childNodes[i].nodeName == tagName)
+        return element.childNodes[i];
+  }
+}
+
+function getHash(url) {
+  var hashPos = url.lastIndexOf('#');
+  return url.substring(hashPos + 1);
+}
