343619f1e6f1a5f6f1be55ddf7a11aa657ffa105
Rami Ahmed Bock <Rami_Ahmed00@hotmail.com>
2013-07-01 13:36:28 +0200

    Added geolocation example code

--- apps/danmarksflyttemandapp/assets/www/cordova.js
+++ apps/danmarksflyttemandapp/assets/www/cordova.js
@@ -8,9 +8,9 @@
  to you under the Apache License, Version 2.0 (the
  "License"); you may not use this file except in compliance
  with the License.  You may obtain a copy of the License at
- 
+
      http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing,
  software distributed under the License is distributed on an
  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -6845,6 +6845,7 @@ require('cordova/channel').onNativeReady.fire();
             break;
         }
     }
+    /*
     // Try to XHR the cordova_plugins.json file asynchronously.
     var xhr = new XMLHttpRequest();
     xhr.onload = function() {
@@ -6871,7 +6872,8 @@ require('cordova/channel').onNativeReady.fire();
         xhr.send();
     } catch(err){
         finishPluginLoading();
-    }
+    }*/
+    finishPluginLoading();
 }(window));
 
 
