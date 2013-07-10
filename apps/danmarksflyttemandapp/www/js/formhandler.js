a7784116ac080f3da4c0caf709d24ef02469e49b
Rami Ahmed Bock <Rami_Ahmed00@hotmail.com>
2013-07-10 19:51:06 +0200

    fixed stupid mistakes

--- apps/danmarksflyttemandapp/www/js/formhandler.js
+++ apps/danmarksflyttemandapp/www/js/formhandler.js
@@ -5,30 +5,32 @@
 function initContactFormHandler() {
     $('#kontakt-send').bind('click', function(evt) {
     evt.preventDefault();
-    $.post('http://danmarksflyttemandapp.herokuapp.com/kontaktform-handler.php', {
+    $.post('http://danmarksflyttemandapp.herokuapp.com/kontaktform-handler.php',
+    {
 
-        Company = $('#company_input').val(),
-        Name = $('#name_input').val(),
-        Address = $('#address_input').val(),
-        City = $('#city_input').val(),
-        Email = $('#email_input').val(),
-        Telephone = $('#telephone_input').val(),
-        Subject = $('#subject_input').val(),
-        Message = $('#message_input').val()
+        Company : $('#company_input').val(),
+        Name : $('#name_input').val(),
+        Address : $('#address_input').val(),
+        City : $('#city_input').val(),
+        Email : $('#email_input').val(),
+        Telephone : $('#telephone_input').val(),
+        Subject : $('#subject_input').val(),
+        Message : $('#message_input').val()
 
-    // HTML function
-    }, function(html) {
-        // Place HTML in string
-        var response = html;
+        // HTML function
+        }, function(html) {
+            // Place HTML in string
+            var response = html;
 
-        // PHP was done and email sent
-        if (response == "success") {
-            $('#kontakt-form').html('<h2>Din besked er sendt!</h2>');
-        }
-        else {
-            navigator.notification.alert('Udfyld venligst alle felter! \n + response');
-            return false;
-        }
+            // PHP was done and email sent
+            if (response == "success") {
+                $('#kontakt-form').html('<h2>Din besked er sendt!</h2>');
+            }
+            else {
+                navigator.notification.alert('Udfyld venligst alle felter! \n + response');
+                return false;
+            }
+        });
     });
 }
 
@@ -36,33 +38,33 @@ function initTilbudFormHandler() {
     $('#tilbud-send').bind('click', function(evt) {
         evt.preventDefault();
         $.post('http://danmarksflyttemandapp.herokuapp.com/tilbudform-handler.php', {
-            Name = $('#name_input').val(),
-            Telephone = $('#telephone_input').val(),
-            Email = $('#email_input').val(),
+            Name : $('#name_input').val(),
+            Telephone : $('#telephone_input').val(),
+            Email : $('#email_input').val(),
 
-            FromAddress = $('#from-address_input').val(),
-            FromZip = $('#from-zipcode_input').val(),
-            FromCity = $('#from-city_input').val(),
-            FromRegion = $('#from-region_input').val(),
-            FromElevator = $('#from-elevator_input').val(),
-            FromParking = $('#from-parking_input').val(),
-            FromSize = $('#from-areal_input').val(),
-            FromRooms = $('#from-rooms_input').val(),
+            FromAddress : $('#from-address_input').val(),
+            FromZip : $('#from-zipcode_input').val(),
+            FromCity : $('#from-city_input').val(),
+            FromRegion : $('#from-region_input').val(),
+            FromElevator : $('#from-elevator_input').val(),
+            FromParking : $('#from-parking_input').val(),
+            FromSize : $('#from-areal_input').val(),
+            FromRooms : $('#from-rooms_input').val(),
 
-            ToAddress = $('#to-address_input').val(),
-            ToZip = $('#to-zipcode_input').val(),
-            ToCity = $('#to-city_input').val(),
-            ToRegion = $('#to-region_input').val(),
-            ToElevator = $('#to-elevator_input').val(),
-            ToParking = $('#to-parking_input').val(),
-            ToSize = $('#to-areal_input').val(),
-            ToRooms = $('#to-rooms_input').val(),
+            ToAddress : $('#to-address_input').val(),
+            ToZip : $('#to-zipcode_input').val(),
+            ToCity : $('#to-city_input').val(),
+            ToRegion : $('#to-region_input').val(),
+            ToElevator : $('#to-elevator_input').val(),
+            ToParking : $('#to-parking_input').val(),
+            ToSize : $('#to-areal_input').val(),
+            ToRooms : $('#to-rooms_input').val(),
 
-            MoveDate = $('#moving-date_input').val(),
-            Flexible = $('#flexible-date_input').val(),
-            AmountMen = $('#amount-men_input').val(),
-            Packing = $('#pack-up_input').val(),
-            Description = $('#description_input').val()
+            MoveDate : $('#moving-date_input').val(),
+            Flexible : $('#flexible-date_input').val(),
+            AmountMen : $('#amount-men_input').val(),
+            Packing : $('#pack-up_input').val(),
+            Description : $('#description_input').val()
 
         }, function(html) {
             var response = html;
@@ -74,12 +76,12 @@ function initTilbudFormHandler() {
                 navigator.notification.alert('Udfyld venligst alle felter! \n + response');
                 return false;
             }
-        }
+        });
     });
-}
+};
 
 function initFormHandler() {
     initContactFormHandler();
     initTilbudFormHandler();
-}
+};
 
