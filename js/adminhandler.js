cf095e61de16811d985ababa0a8408368d122b17
Rami Ahmed Bock <Rami_Ahmed00@hotmail.com>
2013-09-10 17:56:27 +0200

    Removed commented code

--- js/adminhandler.js
+++ js/adminhandler.js
@@ -18,18 +18,4 @@ var createNewBlogPost = function(jsonFormData) {
     $.post('blogs/bloghandler.php', jsonFormData, function(response) {
         console.log("Response: " + response);
     });
-/*
-    var request = $.ajax({
-        type: "post",
-        url: "./blogs/bloghandler.php",
-        data: jsonFormData
-    });
-
-    request.done(function(response, textStatus, jqXHR) {
-        console.log("Success. Response: " + response, textStatus);
-    });
-    request.fail(function(jqXHR, textStatus, errorThrown) {
-        console.log("Error: " + textStatus, errorThrown);
-    });
-*/
 }
