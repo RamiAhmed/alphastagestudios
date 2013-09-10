bd13eebff19659c79dbcf19c39f84d024ae97f20
Rami Ahmed Bock <Rami_Ahmed00@hotmail.com>
2013-09-10 17:14:57 +0200

    Testing generating correct blog file name

--- blogs/bloghandler.php
+++ blogs/bloghandler.php
@@ -1,15 +1,31 @@
 <?php
+    function seoUrl($string) {
+        //lower case everything
+        $string = strtolower($string);
+        //make alphaunermic
+        $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+        //Clean multiple dashes or whitespaces
+        $string = preg_replace("/[\s-]+/", " ", $string);
+        //Convert whitespaces and underscore to dash
+        $string = preg_replace("/[\s_]/", "-", $string);
+        return $string;
+    }
+
     $blog_template = './blog-template.html';
+
+    $today = getdate();
+    $day = $today['mday'];
+    $month = $today['mon'];
+    $year = $today['year'];
 
     $blog_title = $_POST['blog-title'];
     $blog_author = $_POST['blog-author'];
     $blog_email = $_POST['blog-author-email'];
     $blog_body = $_POST['blog-body'];
 
-    $feedback = "Title: $blog_title\n".
-                "Author: $blog_author\n".
-                "Email: $blog_email\n".
-                "Body: $blog_body";
-    echo $feedback;
+
+    $new_blog = "$day-$month-$year" . "_" . seoURL($blog_title);
+
+    echo $new_blog;
 
 ?>
