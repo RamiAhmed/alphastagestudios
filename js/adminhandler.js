6e1a99721648bb1f50445f3895bd51c6de5e50fd
Rami Ahmed Bock <Rami_Ahmed00@hotmail.com>
2013-09-10 23:04:22 +0200

    Placed feedback in its own paragraph

--- js/adminhandler.js
+++ js/adminhandler.js
@@ -23,7 +23,7 @@ var createNewBlogPost = function(jsonFormData) {
         else {
             resultDiv = "<div class='alert alert-danger'>Your post was not created, an error occured.</div>";
         }
-        $(".panel-body").append(resultDiv);
+        $(".panel-body").append("<p>" + resultDiv + "</p>");
         console.log("response: " + response);
     });
 }
