02eeb72c26478117123fb7e32fc7bfa29330b3fb
Rami Ahmed Bock <Rami_Ahmed00@hotmail.com>
2013-09-14 16:18:11 +0200

    Try parsing inner json array

--- js/adminhandler.js
+++ js/adminhandler.js
@@ -54,8 +54,8 @@ var initializeBlogManagementContainer = function() {
         console.log("result stringify: " + stringifiedResult);
         console.log("result stringified and parsed: " + JSON.parse(stringifiedResult));
 
-        var oneBlog = result[0];
-        console.log("oneBlog[0] : " + oneBlog[0]);
+        var oneBlog = JSON.parse(JSON.stringify(result[0]));
+        console.log("oneBlog[0]: " + oneBlog[0]);
         console.log("oneBlog.length: " + oneBlog.length);
 
     });
