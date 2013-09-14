bd66fd2dd2a2489f14a446f93d58a3af7d30adb7
Rami Ahmed Bock <Rami_Ahmed00@hotmail.com>
2013-09-14 16:05:58 +0200

    Tried to fix debug feedback

--- js/adminhandler.js
+++ js/adminhandler.js
@@ -50,9 +50,12 @@ var initializeBlogManagementContainer = function() {
         console.log("result.length: " + result.length);
         console.log("result[0]: " + result[0]);
         console.log("result[0].length: " + result[0].length);
-        console.log("result parsed: " + JSON.parse(result));
-        console.log("result stringify: " + JSON.stringify(result));
-        console.log("result stringified and parsed: " + JSON.parse(JSON.stringify(result)));
+        var parsedResult = JSON.parse(result);
+        console.log("result parsed: " + parsedResult);
+        var stringifiedResult = JSON.stringify(result);
+        console.log("result stringify: " + stringifiedResult);
+        console.log("result stringified and parsed: " + JSON.parse(stringifiedResult));
+        console.log("result parsed and stringified: " + JSON.stringify(parsedResult));
 
 
     });
