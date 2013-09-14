23ed6c4e5f0c4b5bc04a5c2ffc25acde24f42721
Rami Ahmed Bock <Rami_Ahmed00@hotmail.com>
2013-09-14 11:20:51 +0200

    Added swiftmailer for php
    - Also changed to send mails with smtp

--- contacthandler.php
+++ contacthandler.php
@@ -1,42 +1,7 @@
 <?php
-/*
-    require_once  'Mail.php';
 
-    $mail_subject = $_POST['contact-subject'];
-    $mail_from_name = $_POST['contact-sender-name'];
-    $mail_from_email = $_POST['contact-sender-email'];
-    $mail_body = $_POST['contact-body'];
-
-    $from = "$mail_from_name <$mail_from_email>";
-    $to = "Rami Ahmed Bock <rami@alphastagestudios.com>";
-    $subject = $mail_subject;
-    $body = $mail_body;
-
-    $headers = array('From' => $from,
-                     'To' => $to,
-                     'Subject' => $subject);
-
-    $wgSMTP = array(
-        'host' => 'tls://smtp.sendgrid.net',
-        'IDHost' => 'heroku.com',
-        'port' => 587,
-        'username' => getenv("SENDGRID_USERNAME"),
-        'password' => getenv("SENDGRID_PASSWORD"),
-        'auth' => true
-    );
-
-    $smtp = Mail::factory('smtp', $wgSMTP);
-
-    $mail = $smtp->send($to, $headers, $body);
-
-    if (PEAR::isError($mail)) {
-        echo "Error sending mail: " . $mail->getMessage();
-    }
-    else {
-        echo "success";
-    }
-*/
     require './sendgrid-php/SendGrid_loader.php';
+    require './swiftmailer/lib/swift_required.php';
 
     $sendgrid = new SendGrid(getenv("SENDGRID_USERNAME"), getenv("SENDGRID_PASSWORD"));
 
@@ -52,22 +17,24 @@
 
     $from = "$mail_from_email";
     $to = "rami@alphastagestudios.com";
-    $subject = $mail_subject;
-    $body = $mail_body;
+    $subject = "SENT FROM WEB: $mail_subject";
+    $body = "$mail_body";
 
     $mail = new SendGrid\Mail();
     $mail->
         addTo($to)->
         setFrom($from)->
         setSubject($subject)->
-        setText($body);
+        setText($body)->
+        setHTML("<p>Hello World Test!</p>");
 
     if (!$mail) {
         echo "Error with SendGrid\Mail() object";
         return;
     }
 
-    $sendgrid->web->send($mail);
+    //$sendgrid->web->send($mail);
+    $sendgrid->smtp->send($mail);
 
     echo "success";
 
