ca6ca28a3bef17af417b0e5010f4ef4878bd8c36
Rami Ahmed Bock <Rami_Ahmed00@hotmail.com>
2013-09-19 14:18:07 +0200

    Fixes

--- js/blog.js
+++ js/blog.js
@@ -27,7 +27,7 @@ var loadBlogPost = function(filename, callback) {
     });
 };
 
-var createBlogPostLink = function(blogPost, index) {
+var createBlogPostLink = function(blogPost, index, blogFileName, blogFilesArray) {
     var previewEnd = blogPost.indexOf("</p>");
     if (!previewEnd || previewEnd === 0) {
         previewEnd = 500;
@@ -67,7 +67,7 @@ $().ready(function() {
         for (var i = 0; i < blogFilesArray.length; i++) {
             var blogFileName = blogFilesArray[i];
             loadBlogPost(blogFileName, function(blogPost) {
-                createBlogPostLink(blogPost, i);
+                createBlogPostLink(blogPost, i, blogFileName, blogFilesArray);
             });
         }
     });
